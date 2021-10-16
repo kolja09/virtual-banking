ecc331c6490c7beea44331d4583a0f7e559328d3
kolja09 <kolja9062005@gmail.com>
2021-10-16 17:11:11 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css'
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import SigninPage from "./pages/Signin";
 
 const App = () => {
     return (
-        <div className='App'>
-            <BrowserRouter>
+        <Router>
+            <div className='App'>
                 <Switch>
-                    <Route path='/' exact component={Home} />
+                    <Route path='/' exact component={Home}/>
                     <Route path='/signin' exact component={SigninPage}/>
                 </Switch>
-            </BrowserRouter>
-        </div>
+            </div>
+        </Router>
     );
 };
 
